59e1ef07a3cb76657ff5510153b362149781eec4
sameerdiwse <new@mail.com>
2022-11-14 12:53:53 +0530

    this is our new project

---
+++ src/main/java/com/newproject/newproject/NewprojectApplication.java
@@ -0,0 +1,13 @@
+package com.newproject.newproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewprojectApplication.class, args);
+	}
+
+}
